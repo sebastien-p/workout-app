40dbef9e0cc6ca8ac93377d4932c4e22025a0a38
Sebastien Pittion <sebastien.pittion@viseo.com>
2020-09-30 15:22:31 +0200

    Try to make stuff work in a browser as well

--- src/modules/root/root.module.ts
+++ src/modules/root/root.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { Dialogs } from '@ionic-native/dialogs/ngx';
 import { Insomnia } from '@ionic-native/insomnia/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -14,7 +13,6 @@ import { RootRoutingModule } from './root-routing.module';
 @NgModule({
   imports: [BrowserModule, IonicModule.forRoot(), RootRoutingModule],
   providers: [
-    Dialogs,
     Insomnia,
     SplashScreen,
     StatusBar,
